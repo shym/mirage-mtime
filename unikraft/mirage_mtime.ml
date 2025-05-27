5eef05fa5b2ff03473686b143a181ea3203e5d36
fabbing <fabrice@tarides.com>
2025-05-27 17:59:10 +0200

    Add period_ns implementation

--- unikraft/mirage_mtime.ml
+++ unikraft/mirage_mtime.ml
@@ -1,6 +1,7 @@
 (* C stub implemented in mirage-unikraft *)
 external elapsed_ns : unit -> int64 = "caml_get_monotonic_time"
+external uk_time_tick_ns : unit -> int64 = "uk_time_tick_ns"
 
 let elapsed_ns () = elapsed_ns ()
 
-let period_ns () = None (* FIXME ? *)
+let period_ns () = Some (uk_time_tick_ns ())
